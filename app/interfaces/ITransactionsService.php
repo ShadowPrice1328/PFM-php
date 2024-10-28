41bb3b78cf094eb451b242c8dee57f52369404e6
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-10-28 20:40:04 +0100

    Created ITransactionsService interface.

---
+++ app/interfaces/ITransactionsService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace interfaces;
+
+interface ITransactionsService
+{
+    /**
+     * Retrieves all transactions present in the database.
+     *
+     * @return TransactionResponse[] A list of TransactionResponse objects.
+     */
+    public function getTransactions(): array;
+
+    /**
+     * Retrieves a specific transaction by its ID.
+     *
+     * @param string|null $transactionId The ID of the category to retrieve.
+     * @return TransactionResponse|null A CategoryResponse with information about the selected category, or null if not found.
+     */
+    public function getTransactionByTransactionId(?string $transactionId): ?TransactionResponse;
+
+    /**
+     * Adds transaction to database
+     *
+     * @param TransactionAddRequest|null $request Transaction to add
+     * @return TransactionResponse The same details but with generated Guid
+     */
+    public function addTransaction(?TransactionAddRequest $request) : TransactionResponse;
+
+    /**
+     * Updates information about transaction
+     *
+     * @param TransactionUpdateRequest|null $request Transaction to update
+     * @return TransactionResponse CategoryResponse with updated information about category
+     */
+    public function updateTransaction(?TransactionUpdateRequest $request) : TransactionResponse;
+
+    /**
+     * Removes transaction from database
+     *
+     * @param string|null $guid Id of transaction to remove
+     * @return bool True if success, false if error has occured
+     */
+    public function deleteTransaction(?string $guid) : bool;
+
+    /**
+     * Retrives all transactions that match filter parameters
+     *
+     * @param string $filterBy
+     * @param string|null $filterString
+     * @return array A list of filtered TransactionResponses
+     */
+    public function getFilteredTransactions(string $filterBy, ?string $filterString) : array;
+
+    /**
+     * Retrives all transaction made between two dates
+     *
+     * @param string|null $startDate
+     * @param string|null $endDate
+     * @return array A list of selected TransactionResponses
+     */
+    public function getTransactionBetweenTwoDates(?string $startDate, ?string $endDate) : array;
+
+    /**
+     * Retrives all categories names of present transactions
+     *
+     * @return array A list of names of categories
+     */
+    public function getCategoryNamesOfTransactions() : array;
+}
